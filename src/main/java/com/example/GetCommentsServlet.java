d64ae47b37c5ec0e062f65571046620eeab2635f
alexdauphine <32578646+alexdauphine@users.noreply.github.com>
2019-03-21 16:47:18 +0100

    Update GetCommentsServlet.java

--- src/main/java/com/example/GetCommentsServlet.java
+++ src/main/java/com/example/GetCommentsServlet.java
@@ -23,26 +23,17 @@ public class GetCommentsServlet {
 	@Produces(MediaType.TEXT_PLAIN)
 	public String getInformation() throws Exception, IOException {
         //return "Hello World";
-	list();
-	}
-	
-  public String list() {
-	  String s;
-    try 
-   {
-      entityManager.getTransaction().begin();
-      @SuppressWarnings("unchecked")
-      List<BDDCOMMENTS> liste = entityManager.createQuery("from BDDCOMMENTS").getResultList();
-      for (Iterator<BDDCOMMENTS> iterator = BDDCOMMENTS.iterator(); iterator.hasNext();)
-      {
-        BDDCOMMENTS c = (BDDCOMMENTS) iterator.next();
-        //System.out.println(BDDCOMMENTS.getDate() + "\t" + BDDCOMMENTS.getComment() + "\n");
-	s = BDDCOMMENTS.getDate() + "\t" + BDDCOMMENTS.getComment() + "\n";
-      }
-      entityManager.getTransaction().commit();
+	String s;
+	entityManager.getTransaction().begin();
+     	@SuppressWarnings("unchecked")
+      	List<BDDCOMMENTS> liste = entityManager.createQuery("from BDDCOMMENTS").getResultList();
+      	for (Iterator<BDDCOMMENTS> iterator = BDDCOMMENTS.iterator(); iterator.hasNext();)
+      		{
+        		BDDCOMMENTS c = (BDDCOMMENTS) iterator.next();
+        		//System.out.println(BDDCOMMENTS.getDate() + "\t" + BDDCOMMENTS.getComment() + "\n");
+			s = BDDCOMMENTS.getDate() + "\t" + BDDCOMMENTS.getComment() + "\n";
+      		}
+      	entityManager.getTransaction().commit();
 	return s;
-    } catch (Exception e) {
-      entityManager.getTransaction().rollback();
-    }
-  }
+	}
 }
