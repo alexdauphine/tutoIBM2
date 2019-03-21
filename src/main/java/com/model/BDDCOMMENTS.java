905b075d2217d005cb13a939352fee46ffee4a5f
alexdauphine <32578646+alexdauphine@users.noreply.github.com>
2019-03-21 15:42:32 +0100

    Create BDDCOMMENTS.java

---
+++ src/main/java/com/model/BDDCOMMENTS.java
@@ -0,0 +1,40 @@
+package com.model;
+ 
+import javax.enterprise.event.Event;
+import javax.inject.Inject;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.PostRemove;
+ 
+@Entity
+public class BDDCOMMENTS {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="ID")
+    private int id;
+ 
+    @Column(name="DATE")
+    private String date;
+    
+    @Column(name="COMMENT")
+    private String comment;
+ 
+    public String getDate() {
+        return date;
+    }
+    public void setDate(String d) {
+        this.date = d;
+    }
+    public String getComment() {
+        return comment;
+    }
+    public void setComment(String c) {
+        this.comment = c;
+    }
+ 
+    @Override
+    public String toString() {
+        return "BDDCOMMENTS :\tdate=" + date + "\tcomment=" + value + "\n";
+    }
+}
