39391db0c184dd0c0907e12f3cf51a63104ed146
alexdauphine <32578646+alexdauphine@users.noreply.github.com>
2019-03-21 11:46:49 +0100

    Update GetCommentsServlet.java

--- src/main/java/com/example/GetCommentsServlet.java
+++ src/main/java/com/example/GetCommentsServlet.java
@@ -3,9 +3,7 @@ package com.example;
 import java.io.IOException;
 import java.util.Set;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 @Path("/comments")
