3c46627aec2e6ddc222e4ac8e3df14a0f96e12ed
alexdauphine <32578646+alexdauphine@users.noreply.github.com>
2019-03-21 11:38:59 +0100

    Update GetCommentsServlet.java

--- src/main/java/com/example/GetCommentsServlet.java
+++ src/main/java/com/example/GetCommentsServlet.java
@@ -8,10 +8,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-
-// This class define the RESTful API to fetch the database service information
-// <basepath>/api/hello
-
 @Path("/comments")
 public class GetCommentsServlet {
 	@GET
