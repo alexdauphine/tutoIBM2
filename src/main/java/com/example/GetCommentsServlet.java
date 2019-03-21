1921df7f4bdc8c60a243b6ce1b45908133fdfe4d
alexdauphine <32578646+alexdauphine@users.noreply.github.com>
2019-03-21 17:16:43 +0100

    Update GetCommentsServlet.java

--- src/main/java/com/example/GetCommentsServlet.java
+++ src/main/java/com/example/GetCommentsServlet.java
@@ -12,6 +12,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 
 import com.model.BDDCOMMENTS;
 
