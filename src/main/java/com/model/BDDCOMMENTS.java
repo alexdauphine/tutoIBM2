ddb595409aae65efdbd3f0c759c4e2338b92f0b0
alexdauphine <32578646+alexdauphine@users.noreply.github.com>
2019-03-22 11:58:48 +0100

    Update BDDCOMMENTS.java

--- src/main/java/com/model/BDDCOMMENTS.java
+++ src/main/java/com/model/BDDCOMMENTS.java
@@ -4,6 +4,8 @@ import javax.enterprise.event.Event;
 import javax.inject.Inject;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.PostRemove;
  
